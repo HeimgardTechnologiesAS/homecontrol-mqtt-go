99c3c7c17a4248f247ece54eb2031888a2a1f38a
mbalug7 <mario.balug@homecontrol.no>
2021-03-17 17:12:35 +0000

    go: code cleanup and errors wrapping

--- src/examples/on_off/main.go
+++ src/examples/on_off/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 	"os/signal"
 	"syscall"
@@ -13,34 +13,47 @@ import (
 )
 
 func ep1StateChange(ep endpoints.Endpoint, cmd string, msg string) {
-	fmt.Printf("GOT STATE UPDATE EP %s, CMD %s, MSG %s\n", ep.GetID(), cmd, msg)
+	log.Printf("Message received. Endpoint: %s, Command: %s, Message: %s\n", ep.GetID(), cmd, msg)
 	if msg == "1" {
-		ep.SendFeedbackMessage(commands.SP, "1")
+		_ = ep.SendFeedbackMessage(commands.SP, "1")
 	} else {
-		ep.SendFeedbackMessage(commands.SP, "0")
+		_ = ep.SendFeedbackMessage(commands.SP, "0")
 	}
 }
 
 func main() {
-	quit := make(chan int)
-	c := make(chan os.Signal)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<-c
-		quit <- 0
-		fmt.Println("Done")
-		time.Sleep(time.Second * 2)
-		os.Exit(1)
-	}()
 
-	mqttDevice := devices.NewMqttDevice("192.168.8.1", "ttdev1234", "hc", "admin", false)
+	mqttDevice, err := devices.NewMqttDevice("192.168.8.1", "ttdev1234", "hc", "admin", true)
+	if err != nil {
+		log.Printf("failed to create MQTT device: %s\n", err.Error())
+		return
+	}
 
 	ep1 := endpoints.NewOnOffEndpoint("ep1", ep1StateChange)
 
 	mqttDevice.AddEndpoint(ep1)
 
-	mqttDevice.Connect()
+	err = mqttDevice.Connect()
+	if err != nil {
+		log.Printf("failed to connect %s\n", err.Error())
+		return
+	}
 	defer mqttDevice.Disconnect()
 
-	mqttDevice.RunForever(quit)
+	setSignalInterrupt(mqttDevice.GetQuitCh())
+	err = mqttDevice.RunForever()
+	if err != nil {
+		log.Printf("Device stopped unexpectedly. %s", err.Error())
+	}
+}
+
+func setSignalInterrupt(quitCh chan error) {
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		<-c
+		quitCh <- nil
+		time.Sleep(time.Second * 2)
+		os.Exit(1)
+	}()
 }
--- src/internal/pkg/devices/device.go
+++ src/internal/pkg/devices/device.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"homecontrol-mqtt-go/internal/pkg/endpoints"
-	"log"
 	"net"
 	"strings"
 	"time"
@@ -15,8 +14,9 @@ import (
 )
 
 const (
-	TLS_PORT = 8883
-	TCP_PORT = 1883
+	TLS_PORT  = 8883
+	TCP_PORT  = 1883
+	zeroEnpID = "0"
 )
 
 type MqttDevice struct {
@@ -24,6 +24,176 @@ type MqttDevice struct {
 	client    mqtt.Client
 	epZero    *endpoints.ZeroEndpoint
 	endpoints map[string]endpoints.Endpoint
+	quitC     chan error
+}
+
+func NewMqttDevice(hostname string, uid string, username string, password string, isSecure bool) (*MqttDevice, error) {
+
+	device := &MqttDevice{
+		uid:       uid,
+		endpoints: make(map[string]endpoints.Endpoint),
+		quitC:     make(chan error),
+	}
+
+	opts := mqtt.NewClientOptions()
+
+	if isSecure {
+		opts.AddBroker(fmt.Sprintf("ssl://%s:%d", hostname, TLS_PORT))
+	} else {
+		opts.AddBroker(fmt.Sprintf("tcp://%s:%d", hostname, TCP_PORT))
+	}
+	opts.SetClientID(uid)
+	opts.SetUsername(username)
+	opts.SetPassword(password)
+
+	opts.SetDefaultPublishHandler(device.onMessageHandler)
+	opts.OnConnect = device.onConnectHandler
+	opts.OnConnectionLost = device.connectionLostHandler
+
+	if isSecure {
+		tlsConfig, err := fetchCertificate(hostname)
+		if err != nil {
+			return nil, err
+		}
+		opts.SetTLSConfig(tlsConfig)
+	}
+
+	device.client = mqtt.NewClient(opts)
+	device.epZero = endpoints.NewZeroEndpoint(device.uid, zeroEnpID, device.SendConfigs, device.sendMsg)
+
+	return device, nil
+}
+
+func fetchCertificate(hostname string) (*tls.Config, error) {
+
+	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", hostname, TLS_PORT))
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to host: %s", err.Error())
+	}
+
+	client := tls.Client(conn, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	defer client.Close()
+
+	if err := client.Handshake(); err != nil {
+		return nil, fmt.Errorf("fetching certificate client handshake failed: %s", err.Error())
+	}
+
+	cert := client.ConnectionState().PeerCertificates[0]
+	certpool := x509.NewCertPool()
+	certpool.AddCert(cert)
+
+	return &tls.Config{
+		RootCAs:            certpool,
+		MaxVersion:         tls.VersionTLS12,
+		InsecureSkipVerify: true,
+	}, nil
+}
+
+func (obj *MqttDevice) Connect() error {
+	if obj.client == nil {
+		return errors.New("client is not initialized")
+	}
+
+	token := obj.client.Connect()
+	success := token.WaitTimeout(time.Second * 5)
+	if !success {
+		return fmt.Errorf("timeout ocurred while trying to connect to client")
+	}
+	if token.Error() != nil {
+		return fmt.Errorf("got error while connecting to client %s", token.Error())
+	}
+
+	err := obj.subscribeTopic(fmt.Sprintf("%s/#", obj.uid))
+	if err != nil {
+		return err
+	}
+
+	err = obj.sendMsg(fmt.Sprintf("d/%s/0/announce", obj.uid), "")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (obj *MqttDevice) Disconnect() {
+	obj.client.Disconnect(250)
+}
+
+func (obj *MqttDevice) RunForever() error {
+	for {
+		err := <-obj.quitC
+		return err
+	}
+}
+
+func (obj *MqttDevice) GetQuitCh() chan error {
+	return obj.quitC
+}
+
+func (obj *MqttDevice) AddEndpoint(enp endpoints.Endpoint) {
+	enp.SetOwnerID(obj.uid)
+	enp.RegisterSendMsgCb(obj.sendMsg)
+	obj.endpoints[enp.GetID()] = enp
+}
+
+func (obj *MqttDevice) SendConfigs() {
+	// send zero endpoint config
+	obj.epZero.SendConfig(len(obj.endpoints))
+	// send endpoints configs
+	for _, value := range obj.endpoints {
+		value.SendConfig()
+	}
+}
+
+func (obj *MqttDevice) subscribeTopic(topic string) error {
+	token := obj.client.Subscribe(topic, 1, nil)
+	success := token.WaitTimeout(time.Second * 2)
+	if !success {
+		return fmt.Errorf("failed to subscribe topic %s. Timeout occurred", topic)
+	}
+	if token.Error() != nil {
+		return fmt.Errorf("got error in token when subscribing %s", token.Error())
+	}
+	return nil
+}
+
+func (obj *MqttDevice) sendMsg(topic string, msg string) error {
+	token := obj.client.Publish(topic, 1, false, msg)
+	success := token.WaitTimeout(time.Second * 2)
+	if !success {
+		return fmt.Errorf("failed to subscribe topic %s. Timeout occurred", topic)
+	}
+	if token.Error() != nil {
+		return fmt.Errorf(
+			"got error in token when sending message: %s on topic: %s. %s",
+			topic, msg, token.Error(),
+		)
+	}
+	return nil
+}
+
+func (obj *MqttDevice) onMessageHandler(client mqtt.Client, msg mqtt.Message) {
+	epID := parseEndpointID(msg.Topic())
+
+	if epID == zeroEnpID {
+		obj.epZero.HandleMessage(string(msg.Payload()))
+		return
+	}
+
+	if enp, found := obj.endpoints[epID]; found {
+		enp.HandleMessage(parseCommand(msg.Topic()), string(msg.Payload()))
+	}
+}
+
+func (obj *MqttDevice) onConnectHandler(client mqtt.Client) {
+	// do nothing
+}
+
+func (obj *MqttDevice) connectionLostHandler(client mqtt.Client, err error) {
+	obj.quitC <- fmt.Errorf("client with ID %s lost connection. Error: %s", obj.uid, err.Error())
 }
 
 func parseEndpointID(token string) string {
@@ -44,178 +214,4 @@ func parseCommand(token string) string {
 		return token[lDelimeterIn+1:]
 	}
 	return ""
-}
-
-func NewMqttDevice(hostname string, uid string, username string, password string, isSecure bool) *MqttDevice {
-
-	device := &MqttDevice{
-		uid:       uid,
-		endpoints: make(map[string]endpoints.Endpoint),
-	}
-
-	opts := mqtt.NewClientOptions()
-
-	if isSecure {
-		opts.AddBroker(fmt.Sprintf("ssl://%s:%d", hostname, TLS_PORT))
-	} else {
-		opts.AddBroker(fmt.Sprintf("tcp://%s:%d", hostname, TCP_PORT))
-	}
-	opts.SetClientID(uid)
-	opts.SetUsername(username)
-	opts.SetPassword(password)
-
-	opts.SetDefaultPublishHandler(device.onMessageHandler)
-	opts.OnConnect = device.onConnectHandler
-	opts.OnConnectionLost = device.connectionLostHandler
-
-	if isSecure {
-		tlsConfig := NewTlsConfig(hostname)
-		opts.SetTLSConfig(tlsConfig)
-	}
-
-	device.client = mqtt.NewClient(opts)
-	device.epZero = endpoints.NewZeroEndpoint(device.uid, "0", device.SendConfigs, device.sendMsg)
-	return device
-
-}
-
-func NewTlsConfig(hostname string) *tls.Config {
-
-	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", hostname, TLS_PORT))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	client := tls.Client(conn, &tls.Config{
-		InsecureSkipVerify: true,
-	})
-	defer client.Close()
-
-	if err := client.Handshake(); err != nil {
-		log.Fatal(err)
-	}
-
-	cert := client.ConnectionState().PeerCertificates[0]
-
-	certpool := x509.NewCertPool()
-	certpool.AddCert(cert)
-	return &tls.Config{
-		RootCAs:            certpool,
-		MaxVersion:         tls.VersionTLS12,
-		InsecureSkipVerify: true,
-	}
-}
-
-func (obj *MqttDevice) Connect() error {
-
-	fmt.Print("Connecting\n")
-	if obj.client == nil {
-		fmt.Print("Client is nil")
-		return errors.New("client is nil")
-	}
-
-	token := obj.client.Connect()
-	success := token.WaitTimeout(time.Second * 5)
-	fmt.Printf("Here %t, %s", success, token.Error())
-	if !success || token.Error() != nil {
-		fmt.Printf("Failed to connect: %s", token.Error())
-		return token.Error()
-	}
-
-	err := obj.subscribeTopic(fmt.Sprintf("%s/#", obj.uid))
-	if err != nil {
-		return err
-	}
-
-	err = obj.sendMsg(fmt.Sprintf("d/%s/0/announce", obj.uid), "")
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func (obj *MqttDevice) Disconnect() {
-	obj.client.Disconnect(250)
-	fmt.Println("client disconnected")
-}
-
-func (obj *MqttDevice) RunForever(quit chan int) {
-	for {
-		<-quit
-		fmt.Println("quit")
-		break
-	}
-	fmt.Println("run forever done")
-}
-
-func (obj *MqttDevice) onMessageHandler(client mqtt.Client, msg mqtt.Message) {
-	fmt.Printf("Received message: %s from topic: %s\n", msg.Payload(), msg.Topic())
-	epID := parseEndpointID(msg.Topic())
-	fmt.Printf("PARSED EP %s\n", epID)
-	if epID == "" {
-		return
-	}
-	if epID == "0" {
-		obj.epZero.HandleMessage(string(msg.Payload()))
-		return
-	}
-
-	if enp, found := obj.endpoints[epID]; found {
-		enp.HandleMessage(parseCommand(msg.Topic()), string(msg.Payload()))
-	} else {
-		fmt.Println("Endpoint not found in map")
-	}
-
-}
-
-func (obj *MqttDevice) onConnectHandler(client mqtt.Client) {
-	fmt.Printf("Client with ID %s connected\n", obj.uid)
-}
-
-func (obj *MqttDevice) connectionLostHandler(client mqtt.Client, err error) {
-	fmt.Printf("Client with ID %s lost connection\n", obj.uid)
-}
-
-func (obj *MqttDevice) AddEndpoint(enp endpoints.Endpoint) {
-	enp.SetOwnerID(obj.uid)
-	enp.RegisterSendMsgCb(obj.sendMsg)
-	obj.endpoints[enp.GetID()] = enp
-}
-
-func (obj *MqttDevice) SendConfigs() {
-
-	fmt.Printf("Sending configs\n")
-	// send zero endpoint config
-	obj.epZero.SendConfig(len(obj.endpoints))
-	// send endpoints configs
-	for _, value := range obj.endpoints {
-		value.SendConfig()
-	}
-}
-
-func (obj *MqttDevice) subscribeTopic(topic string) error {
-	token := obj.client.Subscribe(topic, 1, nil)
-	success := token.WaitTimeout(time.Second * 2)
-	if !success {
-		return errors.New("failed to subscribe")
-	}
-	if token.Error() != nil {
-		return token.Error()
-	}
-	fmt.Printf("Subscribed to topic: %s\n", topic)
-	return nil
-}
-
-func (obj *MqttDevice) sendMsg(topic string, msg string) error {
-	fmt.Printf("Sending MSG Topic: %s Message: %s\n", topic, msg)
-	token := obj.client.Publish(topic, 1, false, msg)
-	success := token.WaitTimeout(time.Second * 2)
-	if !success {
-		return errors.New("failed to announce")
-	}
-	if token.Error() != nil {
-		return token.Error()
-	}
-	return nil
 }
