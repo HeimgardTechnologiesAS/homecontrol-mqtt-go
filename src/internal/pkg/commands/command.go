b41c12c159701c38e249ab19473e9c916282e6d6
mbalug7 <mario.balug@homecontrol.no>
2021-09-15 19:53:24 +0000

    minor refactoring

--- src/internal/pkg/commands/command.go
+++ src/internal/pkg/commands/command.go
@@ -1,5 +1,19 @@
 package commands
 
+const (
+	CONF = "conf"
+	SP   = "sp" // status power
+	CP   = "cp" // control power
+	ST   = "st" // status temperature
+	SL   = "sl" // stauts level
+	CL   = "cl" // control level
+	CHS  = "chs"
+	SHS  = "shs"
+	CI   = "ci" // control identify
+	SM   = "sm" //status motion
+	SH   = "sh" //status humidity
+)
+
 type command struct {
 	cmdType string
 	state   string
